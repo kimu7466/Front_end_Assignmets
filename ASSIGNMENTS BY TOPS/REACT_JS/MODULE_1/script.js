0673f7de56ae1a496fb80eefcadceef05814b745
kimu7466 <kimu7466@gmail.com>
2024-09-25 22:13:56 +0530

    module_1

---
+++ ASSIGNMENTS BY TOPS/REACT_JS/MODULE_1/script.js
@@ -0,0 +1,9 @@
+function toggleForms() {
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+    
+    loginForm.classList.toggle('hidden');
+    registerForm.classList.toggle('hidden');
+}
+
+
